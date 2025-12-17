20d519dffb4d46c29470c709db302810110d52a3
dev-mes <mespinosasanz@gmail.com>
2025-12-17 23:24:04 +0100

    feat: Upgrade of SEO basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  site: "https://manuelespinosa.dev",
-  integrations: [tailwind()],
+  site: "https://www.manuelespinosa.dev",
+  integrations: [tailwind(), sitemap()],
 });
